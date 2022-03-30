ff1b256e3dce51f4583a63914b20bc15a14e4ef9
Kyle Phillips <kphillips375@yahoo.com>
2022-03-30 03:47:18 -0400

    Added genre to struct

--- main.go
+++ main.go
@@ -13,11 +13,19 @@ type Book struct {
 	Author    string `json:"author"`
 	Title     string `json:"title"`
 	Publisher string `json:"publisher"`
+	Genre	  Genre  `json:"Genre"`
+}
+type Genre struct {
+	Fiction		string	`json:"fiction"`
+	NonFiction	string	`json:"nonfiction"`
+	Drama		string	`json:"drama"`
+	Poetry		string	`json:"poetry"`
 }
 type Repository struct {
 	DB *gorm.DB
 }
 
+// create
 func (r *Repository) CreateBook(context *fiber.Ctx) error {
 	book := Book{}
 	err := context.BodyParser(&book)
@@ -31,7 +39,21 @@ func (r *Repository) CreateBook(context *fiber.Ctx) error {
 		context.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "could not create book"})
 		return err
 	}
-	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "book has been created"})
+	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "book has been created"}) 
+	return nil
+}
+
+// get all books
+func (r *Repository) GetBooks(context *fiber.Ctx) error {
+	bookModels := &[]models.Books{}
+
+	err := r.DB.Find(bookModels).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "could not get books"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "All books have been retrieved", "data": bookModels,
+})
 	return nil
 }
 
@@ -39,6 +61,7 @@ func (r *Repository) CreateBook(context *fiber.Ctx) error {
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/create_books", r.CreateBook)
+	api.Get("/books", r.GetBooks)
 }
 
 func main() {
@@ -48,6 +71,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db, err := storage.NewConnection(config)
 
 	if err != nil {
 		log.Fatal("could not load the database")
